20df7810df8a5e704ac2c809e9fe3e28a2e00c42
MaxChandia <maximiliano.chandiaf@gmail.com>
2026-06-05 03:21:17 -0400

    refactor(views): Limpieza código Taller

--- src/views/Taller.js
+++ src/views/Taller.js
@@ -2,50 +2,62 @@ import React from "react";
 import '../styles/taller.css'
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
+import AnimateOnScroll from "../components/AnimateOnScroll";
 
 const Workplace = () => {
     return(
         <div>
-       <Navbar/>
-       <div className="headerWorkshop" >
-            <img loading="lazy" className= "animate-on-scroll animate-right" src='/photos/taller2.webp' alt="headerWorkshop"></img>
-            <p className="firstTitle animate-on-scroll animate-left">Contamos con equipamiento de vanguardia</p>
-            <p className="secondTitle animate-on-scroll animate-left">Asegurando a nuestros clientes la mejor calidad del mercado</p>
-       </div>
-       <div className="workshopCards">
-        <div className="card animate-on-scroll animate-up">
-                <img loading="lazy" src='/photos/taller-imagen-4.webp' alt="workshop1"></img>
-                <p className="title">Torno CNC</p>
-                <p className="description">720 x 2500</p>
+            <Navbar/>
+            <div className="headerWorkshop">
+                <AnimateOnScroll animation="animate-right">
+                    <img loading="lazy" src='/photos/taller2.webp' alt="headerWorkshop"/>
+                </AnimateOnScroll>
+                <AnimateOnScroll animation="animate-left">
+                    <p className="firstTitle" >Contamos con equipamiento de vanguardia</p>
+                </AnimateOnScroll>
+                <AnimateOnScroll  animation="animate-left" delay={0.5}>
+                    <p className="secondTitle">Asegurando a nuestros clientes la mejor calidad del mercado</p>
+                </AnimateOnScroll>
             </div>
-            <div className="card animate-on-scroll animate-up">
-                <img loading="lazy" src='/photos/taller-imagen-6.webp' alt="workshop6"></img>
-                <p className="title">Torno CNC</p>
-                <p className="description">1030x900x3200</p>
+
+            <div className="workshopCards">
+                <AnimateOnScroll className="card" animation="animate-up">
+                    <img loading="lazy" src='/photos/taller-imagen-4.webp' alt="workshop1"/>
+                    <p className="title">Torno CNC</p>
+                    <p className="description">720 x 2500</p>
+                </AnimateOnScroll>
+                
+                <AnimateOnScroll className="card" animation="animate-up">
+                    <img loading="lazy" src='/photos/taller-imagen-6.webp' alt="workshop6"/>
+                    <p className="title">Torno CNC</p>
+                    <p className="description">1030x900x3200</p>
+                </AnimateOnScroll>
+                
+                <AnimateOnScroll className="card" animation="animate-up">
+                    <img loading="lazy" src='/photos/taller-imagen-2.webp' alt="workshop2"/>
+                    <p className="title">Fresadora Vertical</p>
+                    <p className="description">1700x5000</p>
+                </AnimateOnScroll>
+                
+                <AnimateOnScroll className="card" animation="animate-up">
+                    <img loading="lazy" src='/photos/taller-imagen-3.webp' alt="workshop3"/>
+                    <p className="title">Centro Mecanizado CNC</p>
+                    <p className="description">1020 x 540 x 510 1 Ton</p>
+                </AnimateOnScroll>
+                
+                <AnimateOnScroll className="card" animation="animate-up">
+                    <img loading="lazy" src='/photos/taller-image-1.webp' alt="workshop4"/>
+                    <p className="title">Torno Convencional</p>
+                    <p className="description">700x2000</p>
+                </AnimateOnScroll>
+                
+                <AnimateOnScroll className="card" animation="animate-up">
+                    <img loading="lazy" src='/photos/taller-imagen-5.webp' alt="workshop5"/>
+                    <p className="title">Soldadoras</p>
+                </AnimateOnScroll>
             </div>
-            <div className="card animate-on-scroll animate-up">
-                <img loading="lazy" src='/photos/taller-imagen-2.webp' alt="workshop2"></img>
-                <p className="title">Fresadora Vertical</p>
-                <p className="description">1700x5000</p>
-            </div>
-            <div className="card animate-on-scroll animate-up">
-                <img loading="lazy" src='/photos/taller-imagen-3.webp' alt="workshop3"></img>
-                <p className="title">Centro Mecanizado CNC</p>
-                <p className="description">1020 x 540 x 510 1 Ton</p>
-            </div >
-            <div className="card animate-on-scroll animate-up"  >
-                <img loading="lazy" src='/photos/taller-image-1.webp' alt="workshop4"></img>
-                <p className="title">Torno Convencional</p>
-                <p className="description">700x2000</p>
-                <p className="description"></p>
-            </div>
-            <div className="card animate-on-scroll animate-up">
-                <img loading="lazy" src='/photos/taller-imagen-5.webp' alt="workshop5"></img>
-                <p className="title">Soldadoras</p>
-            </div>
-       </div>
-       <Footer/>
-       </div>
+            <Footer/>
+        </div>
     );
 };
 
