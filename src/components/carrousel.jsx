a80c091b0ed7c5ccda6690b29fd9e42d99a10672
MaxChandia <maximiliano.chandiaf@gmail.com>
2026-06-05 03:22:39 -0400

    refactor(components): cambios en corrousel debido a cambios en terminos de Yputub

--- src/components/carrousel.jsx
+++ src/components/carrousel.jsx
@@ -4,25 +4,28 @@ import { Link } from "react-router-dom";
 
 const Carrousel = () => {
     return(
-        <div className="slideContainer animation-fade-in" >
-               <iframe 
-                width="560" 
-                height="315" 
-                src="https://www.youtube.com/embed/7akUHjWCRNM?autoplay=1&loop=1&playlist=7akUHjWCRNM&controls=0&modestbranding=1&iv_load_policy=3&disablekb=1&mute=1" 
-                frameBorder="0" 
-                allow="autoplay; encrypted-media" 
-                allowFullScreen 
-                title="Maestranza">
-            </iframe>
-            <div className="slideTextContainer">
-            <div className="slideText animate-on-scroll animate-right" >Aportando con excelencia a la industria minera</div>
-            <Link to="/contacto">
-            <button className="slideButton">
-                    Contáctenos
-            </button>
-            </Link>
+    <div className="slideContainer animation-fade-in">
+        <video 
+            className="videoFondo"
+            autoPlay 
+            loop 
+            muted 
+            playsInline
+        >
+            <source src="/eyh_video.mp4" type="video/mp4" />
+        </video>
+        
+        <div className="slideTextContainer">
+            <div className="slideText animate-on-scroll animate-right" >
+                Aportando con excelencia a la industria minera
             </div>
+            <Link to="/contacto">
+                <button className="slideButton">
+                    Contáctenos
+                </button>
+            </Link>
         </div>
+    </div>
     );
 };
 
